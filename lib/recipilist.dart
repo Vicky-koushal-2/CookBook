891ac0d326f4dd5ceb7260943c5c73daa1bbf80c
Vicky kaushal <121958580+Vicky-koushal-2@users.noreply.github.com>
2024-09-04 16:28:48 +0530

    Initial commit

---
+++ lib/recipilist.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Recipilist extends StatefulWidget {
+  const Recipilist({Key? key}) : super(key: key);
+
+  @override
+  State<Recipilist> createState() => _RecipilistState();
+}
+
+class _RecipilistState extends State<Recipilist> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
